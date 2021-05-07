ecb7296c0c09f0463fe69ef6cdd3294397ed3d33
galigaribaldi <galigaribaldi@live.com>
2021-05-07 01:58:52 -0500

    Resumen a cursos terminada

--- main.py
+++ main.py
@@ -2,15 +2,14 @@
 import ModulosInternos.modelsClassroom as cursos
 import ModulosInternos.correo as enviar
 import ModulosInternos.tables as tabla
+
 ##Sistema
 import pandas as pd
-
 
 def convert_allCourses():
     c = cursos.get_all_courses()
     print(c)
     c.to_excel("Cursos.xlsx")
-
 
 listaGrados = {"K2":126380062908,"K3":126382145220,"1ro":126382145238,"2do":126382430010,
                "3ro":126382430032,
@@ -38,9 +37,8 @@ def resumen_Actividades(mes,anio, Grado):
 def resumen_Actividades_tipo(mes,anio, Grado,tipo_actividad):
     n = cursos.courses_list_activities(Grado, mes,anio)
     act = cursos.return_themes_by_name(Grado, tipo_actividad)
-    #print(act.head)
-    #print(n.head)
     d = pd.merge(left=n, right=act, left_on='TopicId', right_on='TopicId')
+    #input()
     users = cursos.get_users_by_idCourse(Grado)
     Final = pd.DataFrame()
     for i in d['CourseWorkID']:
@@ -52,21 +50,118 @@ def resumen_Actividades_tipo(mes,anio, Grado,tipo_actividad):
     return Final3
 
 def resumen_Total_tipo(mes,anio, Grado, GradoStr):
+    ###Primera parte
     allThemes = cursos.return_all_themes(Grado)
-    users = cursos.get_users_by_idCourse(Grado)
-    Resumen = pd.DataFrame()
     with pd.ExcelWriter(GradoStr+"_Resumen"+'.xlsx') as writer:
         for i in allThemes['name']:
+            Resumen = pd.DataFrame()
+            print(i)
             data = resumen_Actividades_tipo(mes,anio, Grado,i)
-            for j in users['StudentID']:
-                nuevo = pd.merge(left=users, right=data,left_on='StudentID', right_on='StudentID')
-                Promedio = nuevo['Calificacion'].mean()
-                print(i, j, "Promedio: ", Promedio)
             data.to_excel(writer,sheet_name=i)
-        
+    #####Segunda parte
+    #input("Segunda parte")
+    ###Leer el Dataframe
+    datosProyectos = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Proyectos")
+    datosExamenes = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Examenes")
+    datosTarea = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Tarea")
+    datosAsistencia = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Asistencia")
+    ### Usuarios
+    users = cursos.get_users_by_idCourse(Grado)
+    ###Generar Resumen
+    DataframeFinal = pd.DataFrame(columns=['StudentID', 'Nombre Alumno', 'Proyecto','Examenes','Tareas','Asistencia', 'Promedio Final'], index=range(len(users['StudentID'])))
+    cont=0
+    for i in range(len(users['StudentID'])):
+        l=[]
+        ########Leyendo y Filtrando Dataframes
+        proyectos = datosProyectos[datosProyectos.StudentID == users['StudentID'][i]]
+        examenes = datosExamenes[datosExamenes.StudentID == users['StudentID'][i]]
+        Tarea = datosTarea[datosTarea.StudentID == users['StudentID'][i]]
+        asistencia = datosAsistencia[datosAsistencia.StudentID == users['StudentID'][i]]
+        ##Agregando valores Generales
+        l.append(users['StudentID'][i]); l.append(users['Student_Name'][i])
+        ###Proyecto (2)
+        l.append(proyectos['Calificacion'].mean())
+        ###Examenes (3)
+        l.append(examenes['Calificacion'].mean())
+        ###Tareas (4)
+        l.append(round(Tarea['Calificacion'].mean(),2))
+        ###Asistencia (5)
+        l.append(asistencia['Calificacion'].mean())
+        ###PromedioFinal
+        PF = (l[2]*.5)+(l[3]*.2)+(l[4]*.15)+(l[5]*.15)
+        l.append(round(PF,2))
+        ###
+        DataframeFinal.iloc[cont] = l
+        cont = cont+1
+    with pd.ExcelWriter(GradoStr+"_Resumen"+'.xlsx') as writer:
+        ###
+        datosProyectos.drop(datosProyectos.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosProyectos.to_excel(writer,sheet_name="Proyectos")
+        ###
+        datosExamenes.drop(datosExamenes.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosExamenes.to_excel(writer,sheet_name="Examenes")
+        ###
+        datosTarea.drop(datosTarea.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosTarea.to_excel(writer,sheet_name="Tarea")
+        ###
+        datosAsistencia.drop(datosAsistencia.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosAsistencia.to_excel(writer,sheet_name="Asistencia")
+        ###
+        DataframeFinal.to_excel(writer,sheet_name="Resumen")
+                   
+d = resumen_Total_tipo(1,2021,listaGrados['6to'],'6to')
 
-d = resumen_Total_tipo(1,2021,listaGrados['4to'],'4to')
-
+def resumeen_Total_tipo_dos(grado,GradoStr):
+    ###Leer el Dataframe
+    datosProyectos = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Proyectos")
+    datosExamenes = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Examenes")
+    datosTarea = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Tarea")
+    datosAsistencia = pd.read_excel(GradoStr+"_Resumen"+".xlsx",sheet_name="Asistencia")
+    ### Usuarios
+    users = cursos.get_users_by_idCourse(grado)
+    ###Generar Resumen
+    DataframeFinal = pd.DataFrame(columns=['StudentID', 'Nombre Alumno', 'Proyecto','Examenes','Tareas','Asistencia', 'Promedio Final'], index=range(len(users['StudentID'])))
+    cont=0
+    for i in range(len(users['StudentID'])):
+        l=[]
+        ########Leyendo y Filtrando Dataframes
+        proyectos = datosProyectos[datosProyectos.StudentID == users['StudentID'][i]]
+        examenes = datosExamenes[datosExamenes.StudentID == users['StudentID'][i]]
+        Tarea = datosTarea[datosTarea.StudentID == users['StudentID'][i]]
+        asistencia = datosAsistencia[datosAsistencia.StudentID == users['StudentID'][i]]
+        ##Agregando valores Generales
+        l.append(users['StudentID'][i]); l.append(users['Student_Name'][i])
+        ###Proyecto (2)
+        l.append(proyectos['Calificacion'].mean())
+        ###Examenes (3)
+        l.append(examenes['Calificacion'].mean())
+        ###Tareas (4)
+        l.append(round(Tarea['Calificacion'].mean(),2))
+        ###Asistencia (5)
+        l.append(asistencia['Calificacion'].mean())
+        ###PromedioFinal
+        PF = (l[2]*.5)+(l[3]*.2)+(l[4]*.15)+(l[5]*.15)
+        l.append(round(PF,2))
+        ###
+        DataframeFinal.iloc[cont] = l
+        cont = cont+1
+    with pd.ExcelWriter(GradoStr+"_Resumen"+'.xlsx') as writer:
+        ###
+        datosProyectos.drop(datosProyectos.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosProyectos.to_excel(writer,sheet_name="Proyectos")
+        ###
+        datosExamenes.drop(datosExamenes.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosExamenes.to_excel(writer,sheet_name="Examenes")
+        ###
+        datosTarea.drop(datosTarea.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosTarea.to_excel(writer,sheet_name="Tarea")
+        ###
+        datosAsistencia.drop(datosAsistencia.filter(regex="Unnamed"),axis=1, inplace=True)
+        datosAsistencia.to_excel(writer,sheet_name="Asistencia")
+        ###
+        DataframeFinal.to_excel(writer,sheet_name="Resumen")
+            
+#resumeen_Total_tipo_dos(listaGrados['4to'],'4to')
 
 def generar_resumen(Clave):
     users = cursos.get_users_by_idCourse(listaGrados[Clave])
